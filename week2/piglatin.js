b5eb14424495515eb74da97fd613121b2314cf50
pgm369 <priissgm@gmail.com>
2023-07-07 21:31:43 -0700

    updates index

---
+++ week2/piglatin.js
@@ -0,0 +1,45 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const converter = new showdown.Converter();
+  
+    // Fetch the README.md content
+    fetch('README.md')
+      .then(response => response.text())
+      .then(data => {
+        const htmlContent = converter.makeHtml(data);
+        const readmeDiv = document.getElementById('readme-content');
+        readmeDiv.innerHTML = htmlContent;
+      })
+      .catch(error => {
+        console.error('Error fetching README.md:', error);
+      });
+  
+    // Prompt the user for input
+    const input = prompt("Enter a sentence or a word:");
+  
+    // Check if the input is not empty or a single-letter word
+    if (input.trim() === "" || input.trim().length === 1) {
+      alert("Input can't be translated.");
+    } else {
+      const words = input.split(" ");
+      const translatedWords = [];
+  
+      for (let word of words) {
+        const lowerCaseWord = word.toLowerCase();
+  
+        if (lowerCaseWord[0].match(/[aeiou]/)) {
+          translatedWords.push(word + "way");
+        } else if (lowerCaseWord[0].match(/[bcdfghjklmnpqrstvwxyz]/)) {
+          if (lowerCaseWord[1].match(/[bcdfghjklmnpqrstvwxyz]/)) {
+            translatedWords.push(word.slice(2) + word.slice(0, 2) + "ay");
+          } else {
+            translatedWords.push(word.slice(1) + word[0] + "ay");
+          }
+        }
+      }
+  
+      const translatedSentence = translatedWords.join(" ");
+      const translationResult = document.getElementById('translation-result');
+      translationResult.textContent = "Translated sentence: " + translatedSentence;
+    }
+  });
+  
