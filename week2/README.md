2df0e5b9398b2d8f2a4066d40713f46c678b63d4
pgm369 <priissgm@gmail.com>
2023-07-07 21:38:57 -0700

    updates index

---
+++ week2/README.md
@@ -0,0 +1,36 @@
+# Pig Latin Translator
+
+## Overview
+The Pig Latin Translator is a simple program that converts words or sentences into Pig Latin based on specific rules.
+
+## Features
+- Translates words into Pig Latin
+- Handles single words and multiple-word phrases
+- Supports words starting with vowels and consonants
+
+## Usage
+1. Open the program in VSCODE or your chosen preference
+2. Enter a sentence or a word when prompted.
+3. The program will apply the Pig Latin rules to each word and display the translated output.
+
+## Examples
+- Input: "apple"
+  Output: "appleway"
+
+- Input: "pig latin"
+  Output: "igpay atinlay"
+
+- Input: "grade"
+  Output: "adegray"
+
+## Contributing
+Contributions are welcome! If you want to contribute to this project, please fork the repository and create a pull request.
+
+## License
+This project is currently not licensed.
+
+## Acknowledgements
+- [JavaScript](https://developer.mozilla.org/en-US/docs/Web/JavaScript) - The programming language used
+
+## Contact
+For any questions or feedback, feel free to reach out to [Priscila Garcia-Mendoza].
--- week2/piglatin.js
+++ week2/piglatin.js
@@ -1,45 +1,39 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const converter = new showdown.Converter();
-  
-    // Fetch the README.md content
-    fetch('README.md')
-      .then(response => response.text())
-      .then(data => {
-        const htmlContent = converter.makeHtml(data);
-        const readmeDiv = document.getElementById('readme-content');
-        readmeDiv.innerHTML = htmlContent;
-      })
-      .catch(error => {
-        console.error('Error fetching README.md:', error);
-      });
-  
-    // Prompt the user for input
-    const input = prompt("Enter a sentence or a word:");
-  
-    // Check if the input is not empty or a single-letter word
-    if (input.trim() === "" || input.trim().length === 1) {
-      alert("Input can't be translated.");
-    } else {
-      const words = input.split(" ");
-      const translatedWords = [];
-  
-      for (let word of words) {
-        const lowerCaseWord = word.toLowerCase();
-  
-        if (lowerCaseWord[0].match(/[aeiou]/)) {
-          translatedWords.push(word + "way");
-        } else if (lowerCaseWord[0].match(/[bcdfghjklmnpqrstvwxyz]/)) {
-          if (lowerCaseWord[1].match(/[bcdfghjklmnpqrstvwxyz]/)) {
-            translatedWords.push(word.slice(2) + word.slice(0, 2) + "ay");
-          } else {
-            translatedWords.push(word.slice(1) + word[0] + "ay");
-          }
-        }
-      }
-  
-      const translatedSentence = translatedWords.join(" ");
-      const translationResult = document.getElementById('translation-result');
-      translationResult.textContent = "Translated sentence: " + translatedSentence;
+// Prompt the user for input
+const input = prompt("Enter a sentence or a word:");
+
+// Check if the input is not empty or a single-letter word
+if (input.trim() === "" || input.trim().length === 1) {
+  console.log("Input can't be translated.");
+} else {
+  // Split the input into individual words
+  const words = input.split(" ");
+
+  // Array to store translated words
+  const translatedWords = [];
+
+  // Iterate over each word and apply Pig Latin rules
+  for (let word of words) {
+    // Convert the word to lowercase for consistent comparison
+    const lowerCaseWord = word.toLowerCase();
+
+    // Check if the word starts with a vowel
+    if (lowerCaseWord[0].match(/[aeiou]/)) {
+      translatedWords.push(word + "way");
     }
-  });
-  
+    // Check if the word starts with a consonant
+    else if (lowerCaseWord[0].match(/[bcdfghjklmnpqrstvwxyz]/)) {
+      // Check if the word starts with two consonants
+      if (lowerCaseWord[1].match(/[bcdfghjklmnpqrstvwxyz]/)) {
+        translatedWords.push(word.slice(2) + word.slice(0, 2) + "ay");
+      } else {
+        translatedWords.push(word.slice(1) + word[0] + "ay");
+      }
+    }
+  }
+
+  // Build the translated sentence
+  const translatedSentence = translatedWords.join(" ");
+
+  // Print the translated sentence
+  console.log(translatedSentence);
+}
